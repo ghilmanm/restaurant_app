42f0c4c5267460621f43c06f62866a2a32fc1fa9
Muhammad Ghilman Firdaus <ghilman6@gmail.com>
2022-08-01 16:02:29 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Restaurant App',
       theme: ThemeData(
         primarySwatch: Colors.purple,
         primaryColor: Colors.white,
